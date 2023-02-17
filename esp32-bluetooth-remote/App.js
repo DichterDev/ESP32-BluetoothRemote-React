1c09c2608fe1946b6937236135ce57441ba99578
DichterDev <david.dr.ruescher@outlook.com>
2023-02-17 16:09:42 +0100

    feat: added basic functionality

--- esp32-bluetooth-remote/App.js
+++ esp32-bluetooth-remote/App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const buttonCodes = {
+  'power': 1,
+  'mute': 2,
+  'decrease': 3,
+  'increase': 4,
+  'line1': 5,
+  'line2': 6,
+  'opt': 7,
+  'cox': 8,
+  'bt': 9,
+  'play': 10,
+  'prev': 11,
+  'next': 12
+};
 
 export default function App() {
+  const [pressedButton, setPressedButton] = useState(0);
+
+  function handleClick(id) {
+    setPressedButton(buttonCodes[id]);
+    return buttonCodes[id];
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{pressedButton}</Text>
+      <Button title='Power' onPress={() => handleClick('power')}></Button>
+      <Button title='Mute' onPress={() => handleClick('mute')}></Button>
+      <Button title='Decrease' onPress={() => handleClick('decrease')}></Button>
+      <Button title='Increase' onPress={() => handleClick('increase')}></Button>
+      <Button title='Line1' onPress={() => handleClick('line1')}></Button>
+      <Button title='Line2' onPress={() => handleClick('line2')}></Button>
+      <Button title='OPT' onPress={() => handleClick('opt')}></Button>
+      <Button title='COX' onPress={() => handleClick('cox')}></Button>
+      <Button title='Bluetooth' onPress={() => handleClick('bt')}></Button>
+      <Button title='Play' onPress={() => handleClick('play')}></Button>
+      <Button title='Previous' onPress={() => handleClick('prev')}></Button>
+      <Button title='Next' onPress={() => handleClick('next')}></Button>
     </View>
   );
 }
