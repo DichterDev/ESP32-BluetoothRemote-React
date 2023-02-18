1d96c05b5d0b8438ef6337a0b766a3ad0e858c52
DichterDev <david.dr.ruescher@outlook.com>
2023-02-18 02:53:25 +0100

    feat: added all needed functionality

--- esp32-bluetooth-remote/App.js
+++ esp32-bluetooth-remote/App.js
@@ -1,6 +1,8 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
 
 const buttonCodes = {
   'power': 1,
@@ -18,37 +20,137 @@ const buttonCodes = {
 };
 
 export default function App() {
-  const [pressedButton, setPressedButton] = useState(0);
 
-  function handleClick(id) {
-    setPressedButton(buttonCodes[id]);
-    return buttonCodes[id];
+  const [iconColor, setIconColor] = useState('#E88873');
+  let intervalCheck = useRef(true);
+  let intervalCheckPrev = useRef(true);
+
+  function resetIconColor() {
+    setIconColor('#E88873');
   }
 
+  async function handleClick(id) {
+    if(!intervalCheck.current && !intervalCheckPrev.current) return;
+    
+    intervalCheck.current = false;
+    setIconColor('red');
+    let code = buttonCodes[id];
+    let res = await axios.post('http://192.168.0.199/api', {
+      cmd: code
+    }, { headers: {
+      'Content-Type': 'application/json'
+    }});
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(intervalCheck.current && intervalCheckPrev.current) {
+        resetIconColor();
+      }
+      intervalCheckPrev.current = intervalCheck.current;
+      intervalCheck.current = true;
+    }, 300);
+
+    return () => clearInterval(interval);
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>{pressedButton}</Text>
-      <Button title='Power' onPress={() => handleClick('power')}></Button>
-      <Button title='Mute' onPress={() => handleClick('mute')}></Button>
-      <Button title='Decrease' onPress={() => handleClick('decrease')}></Button>
-      <Button title='Increase' onPress={() => handleClick('increase')}></Button>
-      <Button title='Line1' onPress={() => handleClick('line1')}></Button>
-      <Button title='Line2' onPress={() => handleClick('line2')}></Button>
-      <Button title='OPT' onPress={() => handleClick('opt')}></Button>
-      <Button title='COX' onPress={() => handleClick('cox')}></Button>
-      <Button title='Bluetooth' onPress={() => handleClick('bt')}></Button>
-      <Button title='Play' onPress={() => handleClick('play')}></Button>
-      <Button title='Previous' onPress={() => handleClick('prev')}></Button>
-      <Button title='Next' onPress={() => handleClick('next')}></Button>
+    <View style={{height: '100%', width: '100%', backgroundColor: '#F3E9D2'}}>
+      <View style={styles.wrapper}>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('power')}>
+            <Feather style={[styles.icon, {color: iconColor}]} name='power'></Feather>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('mute')}>
+            <Feather style={[styles.icon, {color: iconColor}]} name='volume-x'></Feather>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('decrease')}>
+            <Feather style={[styles.icon, {color: iconColor}]} name='volume-1'></Feather>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('increase')}>
+            <Feather style={[styles.icon, {color: iconColor}]} name='volume-2'></Feather>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('line1')}>
+            <Text style={[styles.text, {color: iconColor}]}>Line1</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('line2')}>
+            <Text style={[styles.text, {color: iconColor}]}>Line2</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('opt')}>
+            <Text style={[styles.text, {color: iconColor}]}>OPT</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('cox')}>
+            <Text style={[styles.text, {color: iconColor}]}>COX</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('bt')}>
+            <Feather style={[styles.icon, {color: iconColor}]} name='bluetooth'></Feather>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('play')}>
+            <Feather style={[styles.icon, {color: iconColor}]} name='play'></Feather>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('prev')}>
+            <Feather style={[styles.icon, {color: iconColor}]} name='skip-back'></Feather>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={() => handleClick('next')}>
+            <Feather style={[styles.icon, {color: iconColor}]} name='skip-forward'></Feather>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    margin: 30
+  },
+  container: {
+    height: '16.5%',
+    width: '50%',
+    alignItems: 'center',
+    padding: 5,
+  },
+  button: {
+    height: '100%',
+    aspectRatio: 1,
+
+    backgroundColor: '#484A47',
+    borderRadius: 999,
+
     alignItems: 'center',
     justifyContent: 'center',
   },
+  icon: {
+    fontSize: 75
+  },
+  text: {
+    fontSize: 35,
+    fontWeight: '600'
+  }
 });
